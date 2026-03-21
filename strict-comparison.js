add5de6be6cb5c6507402f156ae36a4737d2a2ac
Mina Ezzat <minaezzat98@gmail.com>
2026-03-21 13:36:45 +0200

    strict comparison problem

---
+++ strict-comparison.js
@@ -0,0 +1,12 @@
+/**
+ * Check if two values are strictly equal (same value & same type)
+ * @param {*} a - first value
+ * @param {*} b - second value
+ * @returns {boolean}
+ */
+
+function strictComparison(a, b) {
+  return a === b;
+}
+
+console.log(strictComparison(10, 10)); // true

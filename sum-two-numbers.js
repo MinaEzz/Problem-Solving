c455a363e1b7e00a92df20ea1a233f56726ece6a
Mina Ezzat <minaezzat98@gmail.com>
2026-03-21 13:31:12 +0200

    sum-two-numbers problem

---
+++ sum-two-numbers.js
@@ -0,0 +1,12 @@
+/**
+ * Sum two numbers
+ * @param {number} a - first number
+ * @param {number} b - second number
+ * @returns {number} sum of a and b
+ */
+
+function sumTwoNumbers(a, b) {
+  return a + b;
+}
+
+console.log(sumTwoNumbers(5, 4)); // 9

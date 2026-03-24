36387879924da77ea8a6aa03a1adfe0bcaba567f
Mina Ezzat <minaezzat98@gmail.com>
2026-03-24 13:03:44 +0200

    get opposite problem

---
+++ opposite-number.js
@@ -0,0 +1,8 @@
+// given an integer or floating number, return its opposite
+
+function opposite(number) {
+  return number * -1;
+}
+
+console.log(opposite(12)); // -12
+console.log(opposite(-5)); // 5

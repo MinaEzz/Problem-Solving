6f4d5db924f65e435cf5b49900f4adf492901eca
Mina Ezzat <minaezzat98@gmail.com>
2026-04-16 11:43:04 +0200

    volume of cuboid problem

---
+++ volume-of-cuboid.js
@@ -0,0 +1,9 @@
+// write a function the atakes length, width and height of cuboid and return its volume
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+console.log(Kata.getVolumeOfCuboid(1, 2, 2)); // 4
